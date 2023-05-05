c645eb301a43b8a4e58b372baac3a25fea782bb9
SuperPhantom <super.phantom.dev@gmail.com>
2023-05-05 13:39:06 -0700

    sub-page

---
+++ pages/oversease-mediclaim/index.jsx
@@ -0,0 +1,253 @@
+import React from 'react'
+import Banner from '../../components/banner/Banner'
+
+export default function index() {
+    return (
+        <>
+
+        <Banner 
+        title="Overseas Mediclaim"
+        description="Business & Holiday Overseas Mediclaim Insurance"
+        />
+            {/* Service Section */}
+            <section className="service_section">
+                <div className="container">
+                    <h3 className="text-center">Overseas Mediclaim Insurance</h3>
+                    <h4 className="text-center text-gray">Please Select The Purpose Of Your Travel Ang Go <a href="#" className="text-gray">(Read Policy)</a> </h4>
+                    {/* devider */}
+                    <div className="row devider">
+                        <div className="col-md-2" />
+                        <div className="col-md-6 text-center">
+                            <div className="devider text-center">
+                                <div className="devider_line" />
+                                <div className="devider_line2" />
+                            </div>
+                        </div>
+                        <div className="col-md-2" />
+                    </div>
+                    <div className="row py-4">
+                        <div className="col-md-3 col-sm-6 text-center">
+                            <div className="card">
+                                <div className="text-center">
+                                    <img src="/images/Group51.png" className="card-img-top" alt="..." />
+                                </div>
+                                <div className="card-body">
+                                    <h6 className>Business/Holiday</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-sm-6 text-center">
+                            <div className="card ">
+                                <div className="text-center">
+                                    <img src="/images/Group50.png" className="card-img-top" alt="..." />
+                                </div>
+                                <div className="card-body">
+                                    <h6 className>Study Travel</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-sm-6 text-center">
+                            <div className="card ">
+                                <div className="text-center">
+                                    <img src="/images/engineer1.png" className="card-img-top" alt="..." />
+                                </div>
+                                <div className="card-body">
+                                    <h6 className>Employment</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-sm-6 text-center">
+                            <div className="card ">
+                                <div className="text-center">
+                                    <img src="/images/Group65.png" className="card-img-top" alt="..." />
+                                </div>
+                                <div className="card-body">
+                                    <h6 className>Frequent Travel</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* End Service Section */}
+            {/* Overview Section */}
+            <section className="overview_section">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="overview_content">
+                                <h3>Overview</h3>
+                                <p>
+                                    It is a long established fact that a reader will be distracted by the readable content of a
+                                    page when
+                                    looking at its layout.
+                                </p>
+                                <p>
+                                    The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+                                    as
+                                    opposed to using 'Content here, content here', making it look like readable English.
+                                </p>
+                                <p>
+                                    Many desktop publishing packages and web page editors now use Lorem Ipsum as their default
+                                    model
+                                    text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
+                                </p>
+                                <p>
+                                    Various versions have evolved over the years, sometimes by accident, sometimes on purpose
+                                    (injected humour and the like).
+                                </p>
+                            </div>
+                            <div className="overview_content">
+                                <h3>Terms &amp; Policy</h3>
+                                <p>
+                                    There are many variations of passages of Lorem Ipsum available, but the majority have
+                                    suffered alteration in some form, by injected humour, or randomised words which don't look
+                                    even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be
+                                    sure there isn't anything embarrassing hidden in the middle of text.
+                                </p>
+                                <p>
+                                </p><ul>
+                                    <li>
+                                        <p>
+                                            But the majority have suffered alteration in some form, by injected humour, or
+                                            randomised words which don't look even slightly believable. If you are going to use
+                                            a passage embarrassing hidden in the middle of text.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <p>
+                                            But the majority have suffered alteration in some form, by injected humour, or
+                                            randomised words which don't look even slightly believable. If you are going to use
+                                            a passage embarrassing hidden in the middle of text.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <p>
+                                            But the majority have suffered alteration in some form, by injected humour, or
+                                            randomised words which don't look even slightly believable. If you are going to use
+                                            a passage embarrassing hidden in the middle of text.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <p>
+                                            But the majority have suffered alteration in some form, by injected humour, or
+                                            randomised words which don't look even slightly believable. If you are going to use
+                                            a passage embarrassing hidden in the middle of text.
+                                        </p>
+                                    </li>
+                                </ul>
+                                <p>
+                                    Various versions have evolved over the years, sometimes by accident, sometimes on purpose
+                                    (injected humour and the like).
+                                </p>
+                            </div>
+                            <div className="overview_content">
+                                <h3>How to Purchase Overseas Mediclaim Insurance</h3>
+                                <p>
+                                    There are many variations of passages of Lorem Ipsum available, but the majority have
+                                    suffered alteration in some form, by injected humour, or randomised words which don't look
+                                    even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be
+                                    sure there isn't anything embarrassing hidden in the middle of text.
+                                </p>
+                                <p>
+                                </p><ul>
+                                    <li>
+                                        <p>
+                                            But the majority have suffered alteration in some form, by injected humour, or
+                                            randomised words which don't look even slightly believable. If you are going to use
+                                            a passage embarrassing hidden in the middle of text.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <p>
+                                            But the majority have suffered alteration in some form, by injected humour, or
+                                            randomised words which don't look even slightly believable. If you are going to use
+                                            a passage embarrassing hidden in the middle of text.
+                                        </p>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="overview_content">
+                                <h3>Overseas Mediclaim Insurance</h3>
+                                <p>
+                                    There are many variations of passages of Lorem Ipsum available, but the majority have
+                                    suffered alteration in some form, by injected humour, or randomised words which don't look
+                                    even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be
+                                    sure there isn't anything embarrassing hidden in the middle of text.
+                                </p>
+                                <p>
+                                </p><ul>
+                                    <li>
+                                        <p>
+                                            <strong className="color-primary">Business/Holiday:</strong> There are many variations
+                                            of passages of Lorem Ipsum available, but the majority have suffered alteration in
+                                            some form, by injected humour, or randomised words which don't look even slightly
+                                            believable. If you are going to use a passage of Lorem Ipsum, you need to be sure
+                                            there isn't anything embarrassing hidden in the middle of text.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <p>
+                                            <strong className="color-primary">Business/Holiday:</strong> There are many variations
+                                            of passages of Lorem Ipsum available, but the majority have suffered alteration in
+                                            some form, by injected humour, or randomised words which don't look even slightly
+                                            believable. If you are going to use a passage of Lorem Ipsum, you need to be sure
+                                            there isn't anything embarrassing hidden in the middle of text.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <p>
+                                            <strong className="color-primary">Business/Holiday:</strong> There are many variations
+                                            of passages of Lorem Ipsum available, but the majority have suffered alteration in
+                                            some form, by injected humour, or randomised words which don't look even slightly
+                                            believable. If you are going to use a passage of Lorem Ipsum, you need to be sure
+                                            there isn't anything embarrassing hidden in the middle of text.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <p>
+                                            <strong className="color-primary">Business/Holiday:</strong> There are many variations
+                                            of passages of Lorem Ipsum available, but the majority have suffered alteration in
+                                            some form, by injected humour, or randomised words which don't look even slightly
+                                            believable. If you are going to use a passage of Lorem Ipsum, you need to be sure
+                                            there isn't anything embarrassing hidden in the middle of text.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <p>
+                                            <strong className="color-primary">Business/Holiday:</strong> There are many variations
+                                            of passages of Lorem Ipsum available, but the majority have suffered alteration in
+                                            some form, by injected humour, or randomised words which don't look even slightly
+                                            believable. If you are going to use a passage of Lorem Ipsum, you need to be sure
+                                            there isn't anything embarrassing hidden in the middle of text.
+                                        </p>
+                                    </li>
+                                </ul>
+                                <p>
+                                    Various versions have evolved over the years, sometimes by accident, sometimes on purpose
+                                    (injected humour and the like).
+                                </p>
+                            </div>
+                            <div className="overview_content">
+                                <h3>Learn More</h3>
+                                <p>
+                                    There are many variations of passages of Lorem Ipsum available, but the majority have
+                                    suffered alteration in some form, by injected humour, or randomised words which don't look
+                                    even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be
+                                    sure there isn't anything embarrassing hidden in the middle of text.
+                                </p>
+                                <p>
+                                    <span className="color-primary">Contact With Our Support:</span> +9945-478-2145 or
+                                    +9954-855-5492
+                                </p>
+                                <p>
+                                </p></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* End Overview Section */}
+
+        </>
+    )
+}
